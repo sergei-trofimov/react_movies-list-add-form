52844eac07e46db131a63742cb9f496408a7a1ab
sergei-trofimov <feast.st@gmail.com>
2021-07-29 15:59:03 +0300

    Updates

---
+++ precache-manifest.11974e9680cf83093fa9b9f79052fd8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b869dca21b6c1e2bde4662ff6ff521b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb43b0fac7db1d75b26f",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "c2618fce8f100492cdd6",
+    "url": "./static/css/main.d7b289fb.chunk.css"
+  },
+  {
+    "revision": "cb43b0fac7db1d75b26f",
+    "url": "./static/js/2.4f4f150b.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.4f4f150b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2618fce8f100492cdd6",
+    "url": "./static/js/main.50ccd232.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.11974e9680cf83093fa9b9f79052fd8b.js"
 );
 
 self.addEventListener('message', (event) => {
